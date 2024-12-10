fe3de7316a99c39182f88bace188717f0a1dd5d2
Giacomo Romagnoli <giacomo.romagnoli4@studio.unibo.it>
2024-12-10 17:20:54 +0100

    build: add multi project helper

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,13 @@
 import io.github.andreabrighi.gradle.gitsemver.conventionalcommit.ConventionalCommit
+import io.github.gciatto.kt.mpp.Plugins
+import io.github.gciatto.kt.mpp.helpers.ProjectType
 
 plugins {
     alias(libs.plugins.dokka)
     alias(libs.plugins.kotlin.jvm)
     alias(libs.plugins.gitSemVer)
     alias(libs.plugins.ktlint)
+    alias(libs.plugins.ktMpp.multiProjectHelper)
 }
 
 buildscript {
@@ -20,6 +23,48 @@ repositories {
 dependencies {
     implementation(rootProject.libs.kotlin.stdlib)
     testImplementation(rootProject.libs.bundles.kotlin.testing)
+}
+
+multiProjectHelper {
+    defaultProjectType = ProjectType.KOTLIN // default project type for all projects which are not explicitly marked
+
+    ktProjects = subprojects.toSet()
+    jvmProjects = emptySet()
+    jsProjects = emptySet()
+    otherProjects = emptySet()
+
+    val baseProjectTemplate =
+        buildSet {
+            add(Plugins.documentation)
+            add(Plugins.linter)
+            add(Plugins.bugFinder)
+            add(Plugins.versions)
+        }
+
+    ktProjectTemplate =
+        buildSet {
+            addAll(baseProjectTemplate)
+            add(Plugins.multiplatform)
+        }
+
+    jvmProjectTemplate =
+        buildSet {
+            addAll(baseProjectTemplate)
+            add(Plugins.jvmOnly)
+        }
+
+    jsProjectTemplate =
+        buildSet {
+            addAll(baseProjectTemplate)
+            add(Plugins.jsOnly)
+        }
+
+    otherProjectTemplate =
+        buildSet {
+            add(Plugins.versions)
+        }
+
+    applyProjectTemplates()
 }
 
 kotlin {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,11 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+    }
+}
+
 plugins {
     id("org.danilopianini.gradle-pre-commit-git-hooks") version "2.0.8"
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
