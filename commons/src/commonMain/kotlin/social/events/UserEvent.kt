c3a414e0e0ded66af6f99060cb55b11c41594c08
Marco Fontana <marco.fontana17@studio.unibo.it>
2024-12-16 17:41:22 +0100

    feat(events): add user events

---
+++ commons/src/commonMain/kotlin/social/events/UserEvent.kt
@@ -0,0 +1,86 @@
+package social.events
+
+import social.ddd.DomainEvent
+
+/**
+ * Interface to represent a user event.
+ */
+interface UserEvent : DomainEvent
+
+/**
+ * Event to represent a user creation.
+ */
+data class UserCreated(
+    val username: String,
+    val email: String,
+): UserEvent {
+    companion object {
+        const val TOPIC = "user-created"
+    }
+}
+
+/**
+ * Event to represent a user deletion.
+ */
+data class UserBlocked(
+    val username: String,
+): UserEvent {
+    companion object {
+        const val TOPIC = "user-blocked"
+    }
+}
+
+/**
+ * Event to represent a user unblock.
+ */
+data class UserUnblocked(
+    val username: String,
+): UserEvent {
+    companion object {
+        const val TOPIC = "user-unblocked"
+    }
+}
+
+/**
+ * Event to represent a user deletion.
+ */
+data class UserLoggedOut(
+    val username: String,
+): UserEvent {
+    companion object {
+        const val TOPIC = "user-logged-out"
+    }
+}
+
+/**
+ * Event to represent admin unblock.
+ */
+data class AdminLoggedOut(
+    val username: String,
+): UserEvent {
+    companion object {
+        const val TOPIC = "admin-logged-out"
+    }
+}
+
+/**
+ * Event to represent a user login.
+ */
+data class UserLoggedIn(
+    val username: String,
+): UserEvent {
+    companion object {
+        const val TOPIC = "user-logged-in"
+    }
+}
+
+/**
+ * Event to represent admin login.
+ */
+data class AdminLoggedIn(
+    val username: String,
+): UserEvent {
+    companion object {
+        const val TOPIC = "admin-logged-in"
+    }
+}
