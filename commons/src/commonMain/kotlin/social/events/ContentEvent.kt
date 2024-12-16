ff9cf49a5914d59026d0ff3e90596db53f842efc
Marco Fontana <marco.fontana17@studio.unibo.it>
2024-12-16 17:47:28 +0100

    feat(events): add content events

---
+++ commons/src/commonMain/kotlin/social/events/ContentEvent.kt
@@ -0,0 +1,27 @@
+package social.events
+
+import social.ddd.DomainEvent
+
+interface ContentEvent : DomainEvent
+
+interface Alert: DomainEvent
+
+data class PostPublished(
+    val title: String,
+    val username: String,
+    val content: String,
+): ContentEvent {
+    companion object {
+        const val TOPIC = "post-published"
+    }
+}
+
+data class ContentAlert(
+    val title: String,
+    val username: String,
+    val content: String,
+): Alert {
+    companion object {
+        const val TOPIC = "content-alert"
+    }
+}
