b355ac1562bdcedb07a63b829b0c939e2d6517f9
Marco Fontana <marco.fontana17@studio.unibo.it>
2024-12-16 17:51:35 +0100

    feat(events): add friendship events

---
+++ commons/src/commonMain/kotlin/social/events/FriendshipEvent.kt
@@ -0,0 +1,32 @@
+package social.events
+
+import social.ddd.DomainEvent
+
+interface FriendshipEvent : DomainEvent
+
+data class FriendshipRequestSent(
+    val sender: String,
+    val receiver: String,
+): FriendshipEvent {
+    companion object {
+        const val TOPIC = "friendship-request-sent"
+    }
+}
+
+data class FriendshipRequestAccepted(
+    val sender: String,
+    val receiver: String,
+): FriendshipEvent {
+    companion object {
+        const val TOPIC = "friendship-request-accepted"
+    }
+}
+
+data class FriendshipRequestRejected(
+    val sender: String,
+    val receiver: String,
+): FriendshipEvent {
+    companion object {
+        const val TOPIC = "friendship-request-rejected"
+    }
+}
