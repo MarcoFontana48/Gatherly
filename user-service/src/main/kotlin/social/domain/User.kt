76c1145ef0ab5d1e05926b1a459a6da3506eed92
Marco Fontana <marco.fontana17@studio.unibo.it>
2024-12-17 18:30:47 +0100

    feat(user-service): add user factory

--- user-service/src/main/kotlin/social/domain/User.kt
+++ user-service/src/main/kotlin/social/domain/User.kt
@@ -1,8 +1,22 @@
 package social.domain
 
 import social.ddd.Entity
+import social.ddd.Factory
 import social.ddd.ID
 
-data class UserID(val userID: String) : ID<String>(userID)
+class User (userID: UserID, private val username: String) : Entity<UserID>(userID)
 
-class User(userID: UserID, private val username: String) : Entity<UserID>(userID)
+data class UserID (val userID: String) : ID<String>(userID)
+
+object UserFactory : Factory<User> {
+    fun userOf(email: String, username: String): User = User(userIDOf(email), username)
+
+    private fun userIDOf(email: String): UserID {
+        // check if the email is valid, if no match is found, returns null, else returns the email as UserID
+        return Regex("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}\$").find(email)?.value?.let {
+            UserID(it)
+        }
+        // if there is no match (null), throw an exception
+            ?: throw IllegalArgumentException("Invalid email")
+    }
+}
--- user-service/src/test/kotlin/social/domain/UserTest.kt
+++ user-service/src/test/kotlin/social/domain/UserTest.kt
@@ -2,8 +2,12 @@ package social.domain
 
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import kotlin.test.assertEquals
 
 class UserTest {
+    private val user = User(UserID("prova@gmail.com"), "username")
+
     @Test
     fun testEquals() {
         val user1 = User(UserID("userID"), "username")
@@ -16,5 +20,16 @@ class UserTest {
         val user1 = User(UserID("userID"), "username")
         val user2 = User(UserID("userID"), "username")
         assertTrue(user1.hashCode() == user2.hashCode())
+    }
+
+    @Test
+    fun testUserCreation() {
+        val actual = UserFactory.userOf("prova@gmail.com", "username")
+        assertEquals(user, actual)
+    }
+
+    @Test
+    fun testFailingUserCreation() {
+        assertThrows<IllegalArgumentException> { UserFactory.userOf("not.an.email", "username") }
     }
 }
