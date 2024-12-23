6915723adaf3d6dc65f6282aabbe287a27f93e5a
Marco Fontana <marco.fontana17@studio.unibo.it>
2024-12-23 11:08:52 +0100

    build: add shadowjar

--- user-service/build.gradle.kts
+++ user-service/build.gradle.kts
@@ -1,18 +1,21 @@
 plugins {
     kotlin("jvm")
     alias(libs.plugins.ktlint)
+    application
+    id("com.github.johnrengelman.shadow") version "8.1.1"
 }
-
-group = "spe.app"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-stdlib")
     implementation(libs.log4j.api)
     implementation(libs.log4j.core)
     implementation(libs.vertx.core)
+    implementation(libs.vertx.web)
+    implementation(libs.vertx.web.client)
     implementation(project(":commons"))
     implementation("mysql:mysql-connector-java:8.0.33")
     testImplementation(kotlin("test"))
@@ -20,8 +23,28 @@ dependencies {
     testImplementation(libs.mockito.core)
 }
 
-tasks.test {
-    useJUnitPlatform()
+project.setProperty("mainClassName", "social.user.MainKt")
+
+tasks {
+    listOf("distZip", "distTar", "startScripts").forEach {
+        named(it) {
+            dependsOn(shadowJar)
+        }
+    }
+
+    test {
+        useJUnitPlatform()
+        exclude("**/UserSQLRepositoryTest.class")
+        exclude("**/RESTUserAPIVerticleTest.class")
+    }
+
+    jar {
+        enabled = false
+    }
+
+    shadowJar {
+        archiveClassifier.set("") // ensure the shadow JAR has no classifier
+    }
 }
 
 kotlin {
