236675d89462a5ef952a8ec245fd11430bed4f21
Marco Fontana <marco.fontana98@outlook.it>
2024-12-30 16:58:22 +0100

    refactor(friendship-service): add method to Message's Factory companion object to create messages with specified ID

--- friendship-service/src/main/kotlin/social/friendship/domain/Message.kt
+++ friendship-service/src/main/kotlin/social/friendship/domain/Message.kt
@@ -1,17 +1,20 @@
-package social.friendship.social.friendship.domain
+package social.friendship.domain
 
 import social.common.ddd.AggregateRoot
 import social.common.ddd.Factory
 import social.common.ddd.ID
+import social.friendship.domain.Message.MessageID
 import java.util.UUID
 
-class Message private constructor(val friendship: Friendship, val content: String) : AggregateRoot<Message.MessageID>(MessageID(UUID.randomUUID())) {
+class Message private constructor(messageId: UUID, val friendship: Friendship, val content: String) : AggregateRoot<MessageID>(MessageID(messageId)) {
     /**
      * Data class to represent the message ID.
      */
     data class MessageID(val value: UUID) : ID<UUID>(value)
 
     companion object : Factory<Message> {
-        fun of(friendship: Friendship, content: String): Message = Message(friendship, content)
+        fun of(friendship: Friendship, content: String): Message = Message(UUID.randomUUID(), friendship, content)
+
+        fun of(messageId: UUID, friendship: Friendship, content: String): Message = Message(messageId, friendship, content)
     }
 }
--- friendship-service/src/test/kotlin/social/friendship/domain/MessageTest.kt
+++ friendship-service/src/test/kotlin/social/friendship/domain/MessageTest.kt
@@ -2,28 +2,52 @@ package social.friendship.domain
 
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertAll
-import social.friendship.social.friendship.domain.Friendship
-import social.friendship.social.friendship.domain.Message
+import social.friendship.social.friendship.domain.User
+import java.util.UUID
 import kotlin.test.assertEquals
 import kotlin.test.assertNotEquals
 
 class MessageTest {
+    private val to = User.of("to")
+    private val from = User.of("from")
+
     @Test
     fun messageCreation() {
-        val message = Message.of(Friendship.of("to", "from"), "content")
+        val message = Message.of(Friendship.of(to, from), "content")
 
         assertAll(
-            { assertEquals("to", message.friendship.to) },
-            { assertEquals("from", message.friendship.from) },
+            { assertEquals(to, message.friendship.to) },
+            { assertEquals(from, message.friendship.from) },
+            { assertEquals("content", message.content) }
+        )
+    }
+
+    @Test
+    fun messageCreationWithSetID() {
+        val id: UUID = UUID.randomUUID()
+        val message = Message.of(id, Friendship.of(to, from), "content")
+
+        assertAll(
+            { assertEquals(id, message.id.value) },
+            { assertEquals(to, message.friendship.to) },
+            { assertEquals(from, message.friendship.from) },
             { assertEquals("content", message.content) }
         )
     }
 
     @Test
     fun eachMessageHasAUniqueIdentifier() {
-        val message1 = Message.of(Friendship.of("to", "from"), "content")
-        val message2 = Message.of(Friendship.of("to", "from"), "content")
+        val message1 = Message.of(Friendship.of(to, from), "content")
+        val message2 = Message.of(Friendship.of(to, from), "content")
 
         assertNotEquals(message1.id, message2.id)
+    }
+
+    @Test
+    fun testSetIdentifier() {
+        val id: UUID = UUID.randomUUID()
+        val message1 = Message.of(id, Friendship.of(to, from), "content")
+
+        assertEquals(id, message1.id.value)
     }
 }
