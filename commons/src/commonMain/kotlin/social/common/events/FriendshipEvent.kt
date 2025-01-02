7808fbaa385684964281683aaf1340e25bd30ca7
Marco Fontana <marco.fontana98@outlook.it>
2025-01-02 20:24:38 +0100

    feat(commons): add FriendshipRemoved event to friendship events

--- commons/src/commonMain/kotlin/social/common/events/FriendshipEvent.kt
+++ commons/src/commonMain/kotlin/social/common/events/FriendshipEvent.kt
@@ -6,6 +6,15 @@ import kotlin.js.JsExport
 @JsExport
 interface FriendshipEvent : DomainEvent
 
+data class FriendshipRemoved(
+    val sender: String,
+    val receiver: String,
+) : FriendshipEvent {
+    companion object {
+        const val TOPIC = "friendship-removed"
+    }
+}
+
 @JsExport
 data class FriendshipRequestSent(
     val sender: String,
