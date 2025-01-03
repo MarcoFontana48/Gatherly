fd438b80e83df50d2db3112cfbdeb44154961b11
Marco Fontana <marco.fontana98@outlook.it>
2025-01-03 16:49:10 +0100

    feat(friendship-service): add main class to boot up the microservice

--- friendship-service/src/main/kotlin/Main.kt
+++ friendship-service/src/main/kotlin/Main.kt
@@ -1,5 +1,12 @@
 package social.friendship
 
+import io.vertx.core.Vertx
+import social.friendship.infrastructure.controller.rest.RESTFriendshipAPIVerticle
+
 fun main(args: Array<String>) {
-    println("Hello, World!")
+    val vertx: Vertx = Vertx.vertx()
+
+    val api = RESTFriendshipAPIVerticle()
+
+    vertx.deployVerticle(api)
 }
