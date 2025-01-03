a5a814d8501882c4d1b2163c06b3118d30ae5702
Marco Fontana <marco.fontana98@outlook.it>
2025-01-03 16:47:40 +0100

    feat(friendship-service): add init.sql file to initialize friendship service SQL database

---
+++ friendship-service/init.sql
@@ -0,0 +1,30 @@
+CREATE TABLE user
+(
+    id VARCHAR(255) NOT NULL PRIMARY KEY
+);
+
+CREATE TABLE friendship_request
+(
+    user_to VARCHAR(255) NOT NULL,
+    user_from VARCHAR(255) NOT NULL,
+    FOREIGN KEY (user_to) REFERENCES user(id),
+    FOREIGN KEY (user_from) REFERENCES user(id),
+    PRIMARY KEY (user_to, user_from)
+);
+
+CREATE TABLE friendship
+(
+    user_to VARCHAR(255) NOT NULL,
+    user_from VARCHAR(255) NOT NULL,
+    FOREIGN KEY (user_to, user_from) REFERENCES friendship_request(user_to, user_from),
+    PRIMARY KEY (user_to, user_from)
+);
+
+CREATE TABLE message
+(
+    id VARCHAR(255) NOT NULL PRIMARY KEY,
+    user_to VARCHAR(255) NOT NULL,
+    user_from VARCHAR(255) NOT NULL,
+    content TEXT NOT NULL,
+    FOREIGN KEY (user_to, user_from) REFERENCES friendship(user_to, user_from)
+);
