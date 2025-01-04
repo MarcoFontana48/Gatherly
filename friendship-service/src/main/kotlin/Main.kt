aba607723c69d9f020e4464dc61fcfb78f8637c6
Marco Fontana <marco.fontana98@outlook.it>
2025-01-04 20:46:18 +0100

    feat(friendship-service): add kafka event verticle to main class

--- friendship-service/src/main/kotlin/Main.kt
+++ friendship-service/src/main/kotlin/Main.kt
@@ -1,12 +1,15 @@
 package social.friendship
 
 import io.vertx.core.Vertx
+import social.friendship.infrastructure.controller.event.KafkaFriendshipEventManagerVerticle
 import social.friendship.infrastructure.controller.rest.RESTFriendshipAPIVerticle
 
 fun main(args: Array<String>) {
     val vertx: Vertx = Vertx.vertx()
 
     val api = RESTFriendshipAPIVerticle()
+    val kafka = KafkaFriendshipEventManagerVerticle()
 
     vertx.deployVerticle(api)
+    vertx.deployVerticle(kafka)
 }
