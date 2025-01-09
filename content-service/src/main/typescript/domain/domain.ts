0a60153ea1e90056ac29f0c9413eddfa3dab4e6b
Giacomo Romagnoli <giacomo.romagnoli4@studio.unibo.it>
2025-01-09 10:46:07 +0100

    feat(content-service): add friendship entity with factory method

--- content-service/src/main/typescript/domain/domain.ts
+++ content-service/src/main/typescript/domain/domain.ts
@@ -69,6 +69,41 @@ class FeedImpl extends AggregateRoot<ID<string>> implements Feed {
     }
 }
 
+export interface Pair<X, Y> {
+    x: X;
+    y: Y;
+}
+
+export class FriendshipID extends ID<Pair<string, string>> {
+    readonly user1: string;
+    readonly user2: string;
+
+    constructor(user1: string, user2: string) {
+        const pair: Pair<string, string> = {x: user1, y: user2}
+        super(pair);
+        this.user1 = user1;
+        this.user2 = user2;
+    }
+}
+
+export interface Friendship extends Entity<FriendshipID> {
+    readonly user1: User;
+    readonly user2: User;
+    readonly id: FriendshipID
+}
+
+class FriendshipImpl extends Entity<FriendshipID> implements Friendship {
+    readonly user1: User;
+    readonly user2: User;
+
+    constructor(user1: User, user2: User) {
+        super(new FriendshipID(user1.email, user2.email));
+        this.user1 = user1;
+        this.user2 = user2;
+    }
+}
+
+
 // Factory methods
 
 export function postFrom(authorName: string, authorEmail: string, content: string, uuid?: string): Post {
@@ -89,4 +124,8 @@ export function feedOf(owner: User, posts: Post[]): Feed {
 
 export function userOf(name: string, email: string): User {
     return new UserImpl(name, email);
+}
+
+export function friendshipOf(user1: User, user2: User): Friendship {
+    return new FriendshipImpl(user1, user2);
 }
