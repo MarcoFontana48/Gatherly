5e1831f3aba794c5340b9b9cacdac655a3c27931
Giacomo Romagnoli <giacomo.romagnoli4@studio.unibo.it>
2025-01-10 17:03:40 +0100

    refactor(content-service): add id specialization for every entity

--- content-service/src/main/typescript/domain/domain.ts
+++ content-service/src/main/typescript/domain/domain.ts
@@ -4,36 +4,38 @@ import AggregateRoot = social.common.ddd.AggregateRoot;
 import ID = social.common.ddd.ID;
 import Entity = social.common.ddd.Entity;
 
-export interface User extends Entity<ID<string>> {
+class UserID extends ID<string> {}
+
+export interface User extends Entity<UserID> {
     readonly userName: string;
     readonly email: string;
-    readonly id: ID<string>;
 }
 
-class UserImpl extends Entity<ID<string>> implements User {
+class UserImpl extends Entity<UserID> implements User {
     readonly userName: string;
     readonly email: string;
 
     constructor(userName: string, email: string) {
-        super(new ID(email));
+        super(new UserID(email));
         this.userName = userName;
         this.email = email;
     }
 }
 
-export interface Post extends AggregateRoot<ID<string>> {
+class PostID extends ID<string> {}
+
+export interface Post extends AggregateRoot<PostID> {
     readonly author: User;
     readonly content: string;
-    readonly id: ID<string>;
     contains(keyword: string): boolean;
 }
 
-class PostImpl extends AggregateRoot<ID<string>> implements Post {
+class PostImpl extends AggregateRoot<PostID> implements Post {
     readonly author: User;
     readonly content: string;
 
     constructor(author: User, content: string, id: string) {
-        super(new ID(id));
+        super(new PostID(id));
         this.author = author;
         this.content = content;
     }
@@ -43,6 +45,8 @@ class PostImpl extends AggregateRoot<ID<string>> implements Post {
     }
 }
 
+class FeedID extends ID<string> {}
+
 export interface Feed {
     readonly posts: Post[];
     readonly owner: User;
@@ -50,12 +54,12 @@ export interface Feed {
     copy(owner: User, posts: Post[]): Feed;
 }
 
-class FeedImpl extends AggregateRoot<ID<string>> implements Feed {
+class FeedImpl extends AggregateRoot<FeedID> implements Feed {
     readonly posts: Post[];
     readonly owner: User;
 
     constructor(owner: User, posts: Post[]) {
-        super(owner.id);
+        super(new FeedID(owner.email));
         this.owner = owner;
         this.posts = posts;
     }
