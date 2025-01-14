f80f7ab44f545ab3e3ba13aed1eaf390a7f089e5
Giacomo Romagnoli <giacomo.romagnoli4@studio.unibo.it>
2025-01-14 17:17:47 +0100

    fix(content-service): fix entrypoint

--- content-service/src/main/typescript/main.ts
+++ content-service/src/main/typescript/main.ts
@@ -1,10 +1,19 @@
-import {social} from "./commons-lib";
-import StatusCode = social.common.endpoint.StatusCode;
 import {Server, DefaultMiddlewares} from "./infrastructure/api"
-import {router} from "./infrastructure/router"
+import {getRouter} from "./infrastructure/router"
+import {ContentServiceImpl} from "./application/service";
+import {
+    SqlFriendshipRepository,
+    SqlPostRepository,
+    SqlUserRepository
+} from "./infrastructure/persistence/sql/sql-repository";
+import {FriendshipRepository, PostRepository, UserRepository} from "./application/repository";
 
-console.log('Hello, world!');
+const userRepository: UserRepository = new SqlUserRepository();
+const postRepository: PostRepository = new SqlPostRepository();
+const friendshipRepository: FriendshipRepository = new SqlFriendshipRepository();
+const service = new ContentServiceImpl(friendshipRepository, postRepository,userRepository);
+service.init().then(() => {
+    const server = new Server(8080, DefaultMiddlewares, getRouter(service));
+    server.start().then(() => console.log("server up!"));
+});
 
-console.log(StatusCode.OK)
-
-new Server(8080, DefaultMiddlewares, router).start().then(() => console.log("server up!"))
