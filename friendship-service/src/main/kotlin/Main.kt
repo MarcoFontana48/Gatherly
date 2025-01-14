07408960fa525fb80d3335cc8ed5b95a2c7f5f9c
Marco Fontana <marco.fontana98@outlook.it>
2025-01-14 17:20:18 +0100

    fix(friendship-service): fix package structures in Main class

--- friendship-service/src/main/kotlin/Main.kt
+++ friendship-service/src/main/kotlin/Main.kt
@@ -2,10 +2,10 @@ package social.friendship
 
 import io.vertx.core.Verticle
 import io.vertx.core.Vertx
-import social.friendship.infrastructure.controller.event.KafkaFriendshipConsumerVerticle
-import social.friendship.application.KafkaFriendshipProducerVerticle
-import social.friendship.infrastructure.controller.rest.RESTFriendshipAPIVerticle
 import social.friendship.application.FriendshipServiceVerticle
+import social.friendship.application.KafkaFriendshipProducerVerticle
+import social.friendship.infrastructure.controller.event.KafkaFriendshipConsumerVerticle
+import social.friendship.infrastructure.controller.rest.RESTFriendshipAPIVerticle
 
 fun main(args: Array<String>) {
     val vertx: Vertx = Vertx.vertx()
