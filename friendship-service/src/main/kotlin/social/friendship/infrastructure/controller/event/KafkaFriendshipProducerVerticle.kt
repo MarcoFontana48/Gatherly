5e394902f34267a257afaafe3a6516d3d286c94a
Marco Fontana <marco.fontana98@outlook.it>
2025-01-14 12:40:45 +0100

    feat(friendship-service): add kafka friendship producer verticle

---
+++ friendship-service/src/main/kotlin/social/friendship/infrastructure/controller/event/KafkaFriendshipProducerVerticle.kt
@@ -0,0 +1,45 @@
+package social.friendship.infrastructure.controller.event
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.KotlinModule
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import io.vertx.core.AbstractVerticle
+import io.vertx.kafka.client.producer.KafkaProducer
+import io.vertx.kafka.client.producer.KafkaProducerRecord
+import org.apache.logging.log4j.LogManager
+import social.common.ddd.DomainEvent
+import social.common.events.FriendshipRemoved
+
+class KafkaFriendshipProducerVerticle : AbstractVerticle() {
+    private val logger = LogManager.getLogger(this::class)
+    private val producerConfig = mapOf(
+        "bootstrap.servers" to "localhost:9092",
+        "key.serializer" to "org.apache.kafka.common.serialization.StringSerializer",
+        "value.serializer" to "org.apache.kafka.common.serialization.StringSerializer",
+        "acks" to "1"
+    )
+    private val mapper: ObjectMapper = jacksonObjectMapper().apply {
+        registerModule(KotlinModule.Builder().build())
+    }
+    private lateinit var producer: KafkaProducer<String, String>
+
+    override fun start() {
+        producer = KafkaProducer.create(vertx, producerConfig)
+    }
+
+    fun publishEvent(event: DomainEvent) {
+        when (event) {
+            is FriendshipRemoved -> publish(FriendshipRemoved.TOPIC, mapper.writeValueAsString(event))
+        }
+    }
+
+    private fun publish(topic: String, value: String, key: String? = null) {
+        val record = KafkaProducerRecord.create<String, String>(
+            topic,
+            key,
+            value
+        )
+        producer.write(record)
+        logger.trace("Published event: TOPIC:{}, KEY:{}, VALUE:{}", topic, key, value)
+    }
+}
