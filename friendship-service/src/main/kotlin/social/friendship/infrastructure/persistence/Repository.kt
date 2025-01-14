2c603ef245dcc597beaa0f985ae8f6c90f4d83b6
Marco Fontana <marco.fontana98@outlook.it>
2025-01-14 12:37:58 +0100

    feat(friendship-service): add interfaces 'UserRepository','FriendshipRequestRepository','FriendshipRepository','MessageRepository'

---
+++ friendship-service/src/main/kotlin/social/friendship/infrastructure/persistence/Repository.kt
@@ -0,0 +1,26 @@
+package social.friendship.social.friendship.infrastructure.persistence
+
+import social.common.ddd.Repository
+import social.friendship.domain.Friendship
+import social.friendship.domain.Friendship.FriendshipID
+import social.friendship.domain.FriendshipRequest
+import social.friendship.domain.FriendshipRequest.FriendshipRequestID
+import social.friendship.domain.Message
+import social.friendship.domain.Message.MessageID
+import social.friendship.social.friendship.domain.User
+import social.friendship.social.friendship.domain.User.UserID
+
+interface UserRepository : Repository<UserID, User>
+
+interface FriendshipRequestRepository : Repository<FriendshipRequestID, FriendshipRequest> {
+    fun getAllFriendshipRequestsOf(userId: UserID): Iterable<FriendshipRequest>
+}
+
+interface FriendshipRepository : Repository<FriendshipID, Friendship> {
+    fun findAllFriendsOf(userID: UserID): Iterable<User>
+}
+
+interface MessageRepository : Repository<MessageID, Message> {
+    fun findAllMessagesReceivedBy(userID: UserID): Iterable<Message>
+    fun findAllMessagesExchangedBetween(user1: UserID, user2: UserID): Iterable<Message>
+}
