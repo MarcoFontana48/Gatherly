5a1f30657b7fadb0b1819190a5d7a804de1b287d
Marco Fontana <marco.fontana98@outlook.it>
2025-01-19 19:28:37 +0100

    build(utils): add shadowJar to runtime elements

---
+++ utils/build.gradle.kts
@@ -0,0 +1,55 @@
+plugins {
+    kotlin("jvm")
+    alias(libs.plugins.ktlint)
+    application
+    id("com.github.johnrengelman.shadow") version "8.1.1"
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-stdlib")
+    implementation(libs.log4j.api)
+    implementation(libs.log4j.core)
+    testImplementation(kotlin("test"))
+}
+
+project.setProperty("mainClassName", "social.utils.MainKt")
+
+tasks {
+    listOf("distZip", "distTar", "startScripts").forEach {
+        named(it) {
+            dependsOn(shadowJar)
+        }
+    }
+
+    test {
+        useJUnitPlatform()
+    }
+
+    jar {
+        enabled = false
+    }
+
+    shadowJar {
+        archiveClassifier.set("") // ensure the shadow JAR has no classifier
+    }
+
+    artifacts {
+        add("runtimeElements", shadowJar)
+    }
+}
+
+java {
+    withSourcesJar()
+    withJavadocJar()
+}
+
+kotlin {
+    jvmToolchain(21)
+    compilerOptions {
+        allWarningsAsErrors = true
+    }
+}
