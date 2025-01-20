796a9d7971a08a0dc0a35412b6f3f8b9e0ff78f5
Giacomo Romagnoli <giacomo.romagnoli4@studio.unibo.it>
2025-01-20 17:16:01 +0100

    fix(content-service): add kafka consumer for event sourcing

--- content-service/src/main/typescript/infrastructure/kafka.ts
+++ content-service/src/main/typescript/infrastructure/kafka.ts
@@ -1,50 +1,63 @@
-import {Kafka} from "kafkajs";
+import {ConsumerConfig, ConsumerSubscribeTopics, Kafka} from "kafkajs";
+import {EventEmitter} from "events";
 import {social} from "../commons-lib";
 import UserCreated = social.common.events.UserCreated;
 import FriendshipRequestAccepted = social.common.events.FriendshipRequestAccepted;
 import {ContentService} from "../application/service";
 import {friendshipOf, userOf} from "../domain/domain";
 
-const kafka = new Kafka({
-    clientId: "content-service",
-    brokers: ["127.0.0.1:9092"],
-    connectionTimeout: 3000,
-    retry: {
-        retries: 5,
-    },
-});
+type Handler = (topic: string, json: any, service: ContentService) => Promise<void>;
 
-const consumer = kafka.consumer({
-    groupId: 'content-service-group',
-});
-
-export async function runConsumer(service: ContentService){
-    await consumer.connect();
-    await consumer.subscribe({
+const defaultSubscription: ConsumerSubscribeTopics =
+    {
         topics: [UserCreated.Companion.TOPIC, FriendshipRequestAccepted.Companion.TOPIC],
         fromBeginning: true,
-    });
-    await consumer.run({
-        autoCommit: false,
-        eachMessage: async ({ topic, partition, message }) => {
-            if(message.value){
-                const json = JSON.parse(message.value.toString());
-                console.log(json);
-                switch (topic) {
-                    case UserCreated.Companion.TOPIC:
-                        await service.addUser(userOf(json.username, json.email));
-                        break;
-                    case FriendshipRequestAccepted.Companion.TOPIC:
-                        // for sql repository username is ignored but consider another strategy for mongo
-                        const user1 = userOf("ignored", json.sender);
-                        const user2 = userOf("ignored", json.receiver);
-                        await service.addFriendship(friendshipOf(user1, user2));
-                        break;
+    }
+
+const defaultHandler: Handler =
+    async (topic: string, json: any, service: ContentService) => {
+        switch (topic) {
+            case UserCreated.Companion.TOPIC:
+                await service.addUser(userOf(json.username, json.email));
+                break;
+            case FriendshipRequestAccepted.Companion.TOPIC:
+                const user1 = userOf("", json.sender);
+                const user2 = userOf("", json.receiver);
+                await service.addFriendship(friendshipOf(user1, user2));
+                break;
+            default:
+                console.log(`unexpected topic: ${topic}`);
+                break;
+        }
+    }
+
+export class KafkaConsumer {
+    private readonly consumer;
+    readonly emitter;
+    private readonly service;
+
+    constructor(kafka: Kafka, consumerConfig: ConsumerConfig, service: ContentService) {
+        this.service = service;
+        this.emitter = new EventEmitter();
+        this.consumer = kafka.consumer(consumerConfig);
+    }
+
+    async consume(subscription = defaultSubscription, handler = defaultHandler) {
+        await this.consumer.connect();
+        await this.consumer.subscribe(subscription);
+        await this.consumer.run({
+            eachMessage:  async ({topic, message}) => {
+                if(message.value){
+                    const json = JSON.parse(message.value.toString());
+                    await handler(topic, json, this.service);
                 }
-            }
-            await consumer.commitOffsets([
-                { topic, partition, offset: (Number(message.offset) + 1).toString() },
-            ]);
-        },
-    });
+                this.emitter.emit("done");
+            },
+        });
+    }
+
+    async stop() {
+        this.consumer.disconnect();
+    }
+
 }
--- content-service/src/test/typescript/infrastructure/kafka.test.ts
+++ content-service/src/test/typescript/infrastructure/kafka.test.ts
@@ -1,25 +1,22 @@
 import {afterEach, beforeEach, describe, expect, test} from "@jest/globals";
+import{EventEmitter} from "events"
+import {Kafka} from "kafkajs";
+import {social} from "../../../main/typescript/commons-lib";
+import UserCreated = social.common.events.UserCreated;
+import FriendshipRequestAccepted = social.common.events.FriendshipRequestAccepted;
+import {KafkaConsumer} from "../../../main/typescript/infrastructure/kafka";
 import {ContentService, ContentServiceImpl} from "../../../main/typescript/application/service";
 import {
     SqlFriendshipRepository,
     SqlPostRepository, SqlUserRepository
 } from "../../../main/typescript/infrastructure/persistence/sql/sql-repository";
-import {Kafka} from "kafkajs";
-import {runConsumer} from "../../../main/typescript/infrastructure/kafka";
-import {social} from "../../../main/typescript/commons-lib";
-import UserCreated = social.common.events.UserCreated;
 import {UserRepository} from "../../../main/typescript/application/repository";
 import {UserID} from "../../../main/typescript/domain/domain";
 
-describe("api module", () => {
+describe("kafka module", () => {
     const shell = require("shelljs");
     let service: ContentService;
     let userRepository: UserRepository;
-    const kafka = new Kafka({
-        clientId: "user-service",
-        brokers: ["127.0.0.1:9092"],
-    });
-    const producer = kafka.producer()
     shell.cd("src/test/typescript/infrastructure");
 
     beforeEach(async () => {
@@ -30,27 +27,88 @@ describe("api module", () => {
             new SqlFriendshipRepository(),
             new SqlPostRepository(),
             userRepository
-        );
+        )
         await service.init(3307);
-        await producer.connect();
     }, 180 * 1000);
 
     afterEach(async () => {
-        await producer.disconnect();
         const result = shell.exec("docker compose down -v");
         console.log(result);
     });
 
-    test("find docker file", async () => {
-        const message = JSON.stringify(new UserCreated("John Doe", "johndoe@example.com"));
+    test("receive a user created topic", async () => {
+        const emitter = new EventEmitter()
+
+        const kafka = new Kafka({
+            clientId: "test",
+            brokers: ["localhost:9092"]
+        })
+
+        const kafka2 = new Kafka({
+            clientId: "redundant",
+            brokers: ["localhost:9092"]
+        })
+
+        const consumer = kafka.consumer({groupId: "content-service-group"})
+        const producer = kafka2.producer();
+        const admin = kafka.admin();
+
+        await admin.connect()
+        await admin.createTopics({topics: [{topic: "topic"}, {topic: "topa"}]})
+        await admin.disconnect()
+
+        await producer.connect()
+        await producer.send({topic: "topic", messages: [{value: "message"}]})
+
+        await consumer.connect()
+        await consumer.subscribe({topics: ["topic", "topa"], fromBeginning: true})
+        await consumer.run({
+            eachMessage: async ({topic, message}) => {
+                console.log(topic);
+                console.log(message?.value);
+                emitter.emit("done");
+            }
+        })
+        return new Promise<void>((resolve) => {
+            emitter.once("done", () => resolve())
+        })
+    }, 90 * 1000);
+
+    test("content-service consumer", async () => {
+        const kafka = new Kafka({
+            clientId: "test",
+            brokers: ["localhost:9092"]
+        });
+
+        const admin = kafka.admin();
+        await admin.connect()
+        await admin.createTopics({
+            topics: [
+                {topic: UserCreated.Companion.TOPIC},
+                {topic: FriendshipRequestAccepted.Companion.TOPIC}
+            ]
+        });
+        await admin.disconnect();
+
+        const producer = kafka.producer();
+        await producer.connect();
         await producer.send({
             topic: UserCreated.Companion.TOPIC,
             messages: [
-                { value: message },
-            ],
+                {
+                    value: JSON.stringify(new UserCreated("John Doe", "johndoe@example.com")),
+                }
+            ]
         });
-        await runConsumer(service);
-        const user = await userRepository.findByID(new UserID("johndoe@example.com"));
+
+        const consumer = new KafkaConsumer(kafka, {groupId: "content-service-group"}, service);
+        await consumer.consume();
+
+        await new Promise<void>((resolve) => {
+            consumer.emitter.once("done", () => resolve())
+        });
+
+        const user = userRepository.findByID(new UserID("johndoe@example.com"));
         expect(user).toBeDefined();
-    });
+    }, 120 * 1000);
 });
