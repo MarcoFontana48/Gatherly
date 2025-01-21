0b1f869115767eb400cebf880028a0e664223596
Marco Fontana <marco.fontana98@outlook.it>
2025-01-21 23:33:28 +0100

    refactor(user-service): main class extracts database credentials from environment

--- user-service/src/main/kotlin/social/user/Main.kt
+++ user-service/src/main/kotlin/social/user/Main.kt
@@ -1,24 +1,35 @@
 package social.user
 
+import io.vertx.core.Verticle
 import io.vertx.core.Vertx
+import social.user.application.KafkaUserProducerVerticle
 import social.user.application.UserServiceImpl
 import social.user.infrastructure.RESTUserAPIVerticle
 import social.user.infrastructure.persitence.sql.UserSQLRepository
+import java.nio.file.Files
+import java.nio.file.Paths
 
 fun main(args: Array<String>) {
     val vertx = Vertx.vertx()
 
     val repository = UserSQLRepository()
     repository.connect(
-        "user-sql-db",
-        "3306",
-        "user",
-        "root",
-        "password"
+        System.getenv("DB_HOST"),
+        System.getenv("DB_PORT"),
+        System.getenv("MYSQL_DATABASE"),
+        System.getenv("MYSQL_USER"),
+        Files.readString(Paths.get("/run/secrets/db_password")).trim(),
     )
+
     val service = UserServiceImpl(repository)
     val api = RESTUserAPIVerticle(service)
+    val producer = KafkaUserProducerVerticle()
 
-    vertx.deployVerticle(service)
-    vertx.deployVerticle(api)
+    deployVerticles(vertx, api, producer, service)
+}
+
+private fun deployVerticles(vertx: Vertx, vararg verticles: Verticle) {
+    verticles.forEach {
+        vertx.deployVerticle(it)
+    }
 }
