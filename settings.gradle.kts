daa88166b8d5d9d00b5486961e67645f0b8b2055
Marco Fontana <marco.fontana98@outlook.it>
2025-02-06 18:04:49 +0100

    chore: remove plugins

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,8 +7,8 @@ pluginManagement {
 }
 
 plugins {
-    id("org.danilopianini.gradle-pre-commit-git-hooks") version "2.0.19"
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+//    id("org.danilopianini.gradle-pre-commit-git-hooks") version "2.0.19"
+//    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
 
 // gitHooks {
